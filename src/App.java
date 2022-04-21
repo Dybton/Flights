13e0f4455a1d40dea17ab4c889263f82116f31a6
Jakob Tolstrup <jakobtolstrup@Jakobs-MacBook-Pro.local>
2022-04-21 09:50:45 +0200

    All test cases work. Will attempt submit

--- src/App.java
+++ src/App.java
@@ -8,11 +8,12 @@ public class App {
 
         // Debugging
         boolean debugScannerFlights = false;
-        boolean debugScannerOperations = true;
         boolean debugDelay = false;
         boolean debugDestination = false;
         boolean debugReroute = false;
-        boolean debugFinal = false;
+        boolean debugFinal = true;
+        boolean debugNext = false;
+        boolean debugCount = false;
 
         // Used for debugging
         ArrayList arr = new ArrayList<String>();
@@ -44,6 +45,7 @@ public class App {
 
         scanner.close();
 
+        // Light test cases
         if (debugScannerFlights) {
             // Expected
             // 09:00:00 Chicago
@@ -86,6 +88,14 @@ public class App {
             // Expected Houston - works
             reroute(redBlackBST, "09:21:05", "Houston");
             System.out.println("-" + destination(redBlackBST, "09:21:05") + "-");
+        }
+        if (debugNext) {
+            // Expected - 09:21:05 Chicago - works
+            System.out.println(next(redBlackBST, "09:20:00"));
+        }
+        if (debugCount) {
+            // Expected 6 - works
+            System.out.println(count(redBlackBST, "09:00:00", "09:03:13"));
         }
         if (debugFinal) {
             // Phoenix
@@ -131,15 +141,25 @@ public class App {
         redBlackBST.put(newTime, destination);
     }
 
-    // Works
     public static void reroute(RedBlackBST redBlackBST, String s, String c) {
         redBlackBST.put(s, c);
     }
 
-    // Works
     public static String destination(RedBlackBST redBlackBST, String s) {
         return (String) redBlackBST.get(s);
+    }
 
+    public static String next(RedBlackBST redBlackBST, String time) {
+        String output = "";
+        String nextDep = (String) redBlackBST.ceiling(time);
+        String nextDest = (String) redBlackBST.get(nextDep);
+        output = nextDep + " " + nextDest;
+        return output;
+    }
+
+    public static String count(RedBlackBST redBlackBST, String time1, String time2) {
+        Integer x = redBlackBST.size(time1, time2);
+        return Integer.toString(x);
     }
 
     public static void operationsRouter(RedBlackBST redBlackBST, String input, ArrayList testArray) {
@@ -155,33 +175,10 @@ public class App {
             delay(redBlackBST, parts[1], parts[2]);
         else if (operation.equals("reroute"))
             reroute(redBlackBST, parts[1], parts[2]);
+        else if (operation.equals("next"))
+            testArray.add(next(redBlackBST, parts[1]));
+        else if (operation.equals("count"))
+            testArray.add(count(redBlackBST, parts[1], parts[2]));
 
     }
-
-    // // lets make some simple code
-    // public static void timeTick() {
-    // }
-
-    // // Helper function that should be able to convert increase and decrease time
-    // public static void changeTime(String time, int delayInSeconds) {
-
-    // // Seperate by :
-    // String[] timeSplitted = time.split(":");
-    // // Convert into int
-    // int hours = Integer.parseInt(timeSplitted[0]);
-    // int minutes = Integer.parseInt(timeSplitted[1]);
-    // int seconds = Integer.parseInt(timeSplitted[2]);
-
-    // // Add func that adds a zero
-
-    // if (seconds >= 60) {
-    // System.out.println(delayInSeconds / 60);
-    // }
-
-    // // Convert into string again
-    // String output = hours + ":" + minutes + ":" + seconds;
-    // System.out.println(output);
-
-    // }
-
 }
